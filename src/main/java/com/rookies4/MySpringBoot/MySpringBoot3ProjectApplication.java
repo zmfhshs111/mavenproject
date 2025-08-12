d034dfcd849c444a038c621ab16ab0575c586659
zmfhshs111 <zmfhshs@cu.ac.kr>
2025-08-12 14:04:13 +0900

    init

---
+++ src/main/java/com/rookies4/MySpringBoot/MySpringBoot3ProjectApplication.java
@@ -0,0 +1,13 @@
+package com.rookies4.MySpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySpringBoot3ProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MySpringBoot3ProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rookies4/MySpringBoot/MySpringBoot3ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rookies4.MySpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySpringBoot3ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
